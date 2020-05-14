ca1f1c34c30930d8f804f1c0323f327a4e2a0661
brenomatos <breno.ms1997@gmail.com>
2020-05-14 15:28:22 -0300

    adiciona pasring de json de entrada e execução do script

--- init_crawler.py
+++ init_crawler.py
@@ -1,5 +1,8 @@
 import sys
 import json
+import os
+
+USERS_FILENAME = "breno.txt"
 
 def get_input_json():
     try:
@@ -11,9 +14,32 @@ def get_input_json():
     return input_json
 
 
+def create_users_input_file(input_json_data,aux_users_filename):
+    users_list = input_json_data["users"]
+    with open(aux_users_filename,"w") as f:
+        for user in users_list:
+            f.write(user+"\n")
+
+def init_comandline_crawler(sleep_time, min_date, aux_users_filename):
+    command = "./run_crawl.sh -p " +str(aux_users_filename)+" -d "+ str(min_date)
+    if(sleep_time is not None):
+        command = command + " -t " + str(sleep_time)
+
+    # print(command)
+    os.system(command)
+
+def parse_json(input_json):
+    with open(input_json, "r") as f:
+        input_json_data = json.load(f)
+        
+    sleep_time = input_json_data["sleep_time"]
+    min_date = input_json_data["min_date"]
+    create_users_input_file(input_json_data ,USERS_FILENAME)
+    init_comandline_crawler(sleep_time,min_date,USERS_FILENAME)
+    os.remove(USERS_FILENAME)
+
 
 input_json = get_input_json()
+parse_json(input_json)
 
-with open(input_json, "r") as f:
-    data = json.load(f)
-    print(data["users"])
+# ./run_crawl.sh -p aux.txt -d "2020,4,25"
