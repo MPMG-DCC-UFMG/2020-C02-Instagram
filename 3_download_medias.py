f04deb1a93c455e649e0d5bdc3cbe5d496dda31c
brenomatos <breno.ms1997@gmail.com>
2020-05-18 14:58:02 -0300

    adiciona modulo para download de medias

---
+++ 3_download_medias.py
@@ -0,0 +1,137 @@
+from sys import argv
+import json
+import requests
+import instaloader
+import os
+
+
+def twitter(post, users, path):
+    """
+    Baixa as mídias de um dado post no twitter, se for de um
+    usuário em users.
+
+    Parametros
+    ----------
+    post : dict
+        Um post de twitter que se deseja baixar as mídias (se existirem)
+    users : set
+        Set de screen names de usuários cujas mídias devem se baixadas
+        Se users for None, todos os usuários serão considerados.
+    path : str
+        O diretório no qual se deseja salvar as mídias.
+    """
+    try:
+        path += '/' + post['id'] + '-'
+        number = 0
+        if users is None or post['screen_name'] in users:
+            for photo in post['medias']:
+                if photo['type'] == 'photo':
+                    filename = path + str(number) + '.jpg'
+                elif photo['type'] == 'video':
+                    filename = path + str(number) + '.mp4'
+                else:
+                    # TODO
+                    continue
+
+                r = requests.get(photo['url'])
+                with open(filename, 'wb') as f:
+                    f.write(r.content)
+
+                number += 1
+
+    except:
+        return False
+
+
+def instagram(post, users, path):
+    """
+    Baixa as mídias de um dado post no instagram, se for de um
+    usuário em users.
+
+    Parametros
+    ----------
+    post : dict
+        Um post de twitter que se deseja baixar as mídias (se existirem)
+    users : set
+        Set de screen names de usuários cujas mídias devem se baixadas
+        Se users for None, todos os usuários serão considerados.
+    path : str
+        O diretório no qual se deseja salvar as mídias.
+    """
+    try:
+        path += post['id']
+        if users is None or post['owner_username'] in users:
+            if post['is_video']:
+                filename = path + '.mp4'
+                iloader = instaloader.Instaloader()
+                get_post = instaloader.Post.from_shortcode(
+                    iloader.context, post['short_code'])
+                url = get_post.video_url
+            else:
+                filename = path + '.jpg'
+                url = post['image_url']
+
+            r = requests.get(url)
+            with open(filename, 'wb') as f:
+                f.write(r.content)
+        return True
+
+    except:
+        return False
+
+
+# Leitura do json de entrada
+if len(argv) != 2:
+    raise Exception(
+        "Número incorreto de parâmetros.\nUse: python3 download.py <input-json>")
+
+with open(argv[1], 'r') as f:
+    input_json = json.load(f)
+
+# Leitura do json de saída
+try:
+    out = input_json['output']
+except:
+    folder = str(max([int(x) for x in os.listdir("data/archives")
+                      if x != 'staging' and x[0] != '.']))
+    out = 'data/archives/' + folder + '/medias.json'
+
+with open(out, 'r') as f:
+    output_json = json.load(f)
+
+# Leitura da pasta onde salvar as imagens
+try:
+    path = input_json['output_media']
+    if path[-1] != '/':
+        path += '/'
+except:
+    folder = str(max([int(x) for x in os.listdir("data/archives")
+                      if x != 'staging' and x[0] != '.']))
+    if not os.path.exists("data/archives/"+folder+'/images'):
+        os.makedirs("data/archives/"+folder+'/images')
+    path = "data/archives/"+folder+'/images/'
+
+# Leitura adicional
+try:
+    is_twitter = input_json['crawler'] == 'twitter'
+    data = output_json['data']
+except:
+    raise Exception("Arquivo não compatível.")
+
+try:
+    users = set(input_json['users_to_download_media'])
+except:
+    users = None
+
+
+# Download das mídias
+count = 0
+for post in data:
+    status = twitter(post, users, path) if is_twitter \
+        else instagram(post, users, path)
+
+    if not status:
+        count += 1
+
+if count > 0:
+    print("Aviso:", count, "posts não reconhecidos")
--- init_crawler.py
+++ init_crawler.py
@@ -4,6 +4,7 @@ import os
 
 USERS_FILENAME = "breno.txt"
 
+
 def get_input_json():
     try:
         input_json = sys.argv[1]
@@ -14,32 +15,42 @@ def get_input_json():
     return input_json
 
 
-def create_users_input_file(input_json_data,aux_users_filename):
+def create_users_input_file(input_json_data, aux_users_filename):
     users_list = input_json_data["users"]
-    with open(aux_users_filename,"w") as f:
+    with open(aux_users_filename, "w") as f:
         for user in users_list:
             f.write(user+"\n")
 
+
 def init_comandline_crawler(sleep_time, min_date, aux_users_filename):
-    command = "./run_crawl.sh -p " +str(aux_users_filename)+" -d "+ str(min_date)
+    command = "./run_crawl.sh -p " + \
+        str(aux_users_filename)+" -d " + str(min_date)
     if(sleep_time is not None):
         command = command + " -t " + str(sleep_time)
 
-    # print(command)
     os.system(command)
+
 
 def parse_json(input_json):
     with open(input_json, "r") as f:
         input_json_data = json.load(f)
-        
+
     sleep_time = input_json_data["sleep_time"]
     min_date = input_json_data["min_date"]
-    create_users_input_file(input_json_data ,USERS_FILENAME)
-    init_comandline_crawler(sleep_time,min_date,USERS_FILENAME)
+    create_users_input_file(input_json_data, USERS_FILENAME)
+    init_comandline_crawler(sleep_time, min_date, USERS_FILENAME)
     os.remove(USERS_FILENAME)
+
+
+def download_medias(input_json):
+    print("======================")
+    print("DOWNLOADING MEDIAS")
+    print("======================")
+    command = "python3 3_download_medias.py " + str(input_json)
+    os.system(command)
 
 
 input_json = get_input_json()
 parse_json(input_json)
-
+download_medias(input_json)
 # ./run_crawl.sh -p aux.txt -d "2020,4,25"
