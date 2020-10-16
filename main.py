b8cf98902cba86d49cc32dbbfa801eb929a6031a
Josemar <josemar.caetano@hotmail.com>
2020-10-15 21:41:50 -0300

    Descrição de classes adicionadas.

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ from data_collection import DataCollection
 import json
 
 
-TARGET_JSON_FOLDER = "/var/instagram-crawler/jsons/"
-INPUT_JSON_FOLDER = "/var/instagram-crawler/"
+# TARGET_JSON_FOLDER = "/var/instagram-crawler/jsons/"
+# INPUT_JSON_FOLDER = "/var/instagram-crawler/"
+
+TARGET_JSON_FOLDER = "/data/jsons/"
+INPUT_JSON_FOLDER = "/data/"
 
 try:
     os.makedirs(TARGET_JSON_FOLDER)
