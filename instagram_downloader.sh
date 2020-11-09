d3cd0f9ea069f0917ea76e3999e64b278540267c
Josemar <josemar.caetano@hotmail.com>
2020-11-09 00:29:16 -0300

    Preparacao para persistencia de dados pelo Kafka.

--- instagram_downloader.sh
+++ instagram_downloader.sh
@@ -5,13 +5,13 @@ cp exemplos/input_instagram.json /data/
 if [ $# -eq 0 ]
   then
     echo "Arquivo ou json de entrada nao informado. Executando coleta com arquivo de entrada padrao."
-    docker run -v "/data:/var/instagram-crawler" --rm -it instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py input_instagram.json
+    docker run -v "/data:/var/instagram-crawler" --rm instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py input_instagram.json
 fi
 if [ $# -eq 1 ]
   then
-    docker run -v "/data:/var/instagram-crawler" --rm -it instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py "$1"
+    docker run -v "/data:/var/instagram-crawler" --rm instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py "$1"
 fi
 if [ $# -eq 2 ]
   then
-    docker run -v "/data:/var/instagram-crawler" --rm -it instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py -d "$2"
+    docker run -v "/data:/var/instagram-crawler" --rm instagram_mp_v1 python3 /home/mp/coletor-instagram/main.py -d "$2"
 fi
